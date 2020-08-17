5645475923bbb04258701fc88f691d8feb6a881a
Simon Warta <simon@warta.it>
2020-08-17 18:59:05 +0200

    Make pubkey more explicit

--- examples/drand-verify.rs
+++ examples/drand-verify.rs
@@ -3,10 +3,11 @@ use std::env;
 
 use drand_verify::{g1_from_fixed, verify};
 
-const PK: [u8; 48] = hex!("868f005eb8e6e4ca0a47c8a77ceaa5309a47978a7c71bc5cce96366b5d7a569937c529eeda66c7293784a9402801af31");
+/// Public key League of Entropy Mainnet (curl -sS https://drand.cloudflare.com/info)
+const PK_LEO_MAINNET: [u8; 48] = hex!("868f005eb8e6e4ca0a47c8a77ceaa5309a47978a7c71bc5cce96366b5d7a569937c529eeda66c7293784a9402801af31");
 
 fn main() {
-    let pk = g1_from_fixed(PK);
+    let pk = g1_from_fixed(PK_LEO_MAINNET);
 
     let args: Vec<String> = env::args().collect();
     if args.len() != 3 {
