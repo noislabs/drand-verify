4ba9590b05cce426768274db67e5b3a604e159d9
Simon Warta <simon@warta.it>
2020-08-18 16:15:01 +0200

    Add tests g1_from_variable_works/g2_from_variable_works

--- src/points.rs
+++ src/points.rs
@@ -51,3 +51,36 @@ pub fn g2_from_variable(data: &[u8]) -> Result<G2Affine, InvalidPoint> {
 pub fn g2_from_fixed(data: [u8; 96]) -> G2Affine {
     G2Compressed(data).into_affine().unwrap()
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn g1_from_variable_works() {
+        let result = g1_from_variable(&hex::decode("868f005eb8e6e4ca0a47c8a77ceaa5309a47978a7c71bc5cce96366b5d7a569937c529eeda66c7293784a9402801af31").unwrap());
+        assert!(result.is_ok());
+
+        let result = g1_from_variable(&hex::decode("868f005eb8e6e4ca0a47c8a77ceaa5309a47978a7c71bc5cce96366b5d7a569937c529eeda66c7293784a9402801af").unwrap());
+        match result.unwrap_err() {
+            InvalidPoint::InvalidLength { expected, actual } => {
+                assert_eq!(expected, 48);
+                assert_eq!(actual, 47);
+            }
+        }
+    }
+
+    #[test]
+    fn g2_from_variable_works() {
+        let result = g2_from_variable(&hex::decode("82f5d3d2de4db19d40a6980e8aa37842a0e55d1df06bd68bddc8d60002e8e959eb9cfa368b3c1b77d18f02a54fe047b80f0989315f83b12a74fd8679c4f12aae86eaf6ab5690b34f1fddd50ee3cc6f6cdf59e95526d5a5d82aaa84fa6f181e42").unwrap());
+        assert!(result.is_ok());
+
+        let result = g2_from_variable(&hex::decode("82f5d3d2de4db19d40a6980e8aa37842a0e55d1df06bd68bddc8d60002e8e959eb9cfa368b3c1b77d18f02a54fe047b80f0989315f83b12a74fd8679c4f12aae86eaf6ab5690b34f1fddd50ee3cc6f6cdf59e95526d5a5d82aaa84fa6f181e").unwrap());
+        match result.unwrap_err() {
+            InvalidPoint::InvalidLength { expected, actual } => {
+                assert_eq!(expected, 96);
+                assert_eq!(actual, 95);
+            }
+        }
+    }
+}
